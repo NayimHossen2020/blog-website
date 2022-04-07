c57371c848bc4fa42ba1c6f3cd582577dc37b107
Nayim hossen <nayimh2015@gmail.com>
2022-04-07 18:08:44 +0600

    support section complite

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import About from './Components/About/About';
 import Hero from './Components/Hero/Hero';
 import Navbar from './Components/Navbar/Navbar';
+import Support from './Components/Support/Support';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar></Navbar>
       <Hero></Hero>
       <About></About>
+      <Support></Support>
     </>
   );
 }
